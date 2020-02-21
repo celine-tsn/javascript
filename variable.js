bac5cca62f737c1672a8e0e9b50a0deb0a80e4a2
Céline <toussainceline@gmail.com>
2020-02-21 12:08:06 +0100

    message de commit

---
+++ variable.js
@@ -0,0 +1,10 @@
+let age=21
+console.log(age)
+let anneedenaissance=1998
+console.log(anneedenaissance)
+let somme= age + anneedenaissance
+console.log(somme)
+let prenom= "Céline"
+let nom= "Toussain"
+let prenomnom= prenom +" "+ nom
+console.log(prenomnom)
